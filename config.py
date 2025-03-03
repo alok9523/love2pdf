ff8c2f1caed54775feda43d78fa441405659eade
alok9523 <97608103+alok9523@users.noreply.github.com>
2025-03-03 20:31:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,8 @@
 
 BOT_TOKEN = "7889602638:AAFtqbV5K_4uCEJcCL4DmdnkdJ0QCzVjrtg"  # Replace with your Telegram bot token
 
+ADMIN_ID = int(os.getenv("ADMIN_ID", 6195379665))  # Replace 123456789 with your actual admin ID
+
 # Database settings
 DB_PATH = "database/users.db"
 
