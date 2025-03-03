b12fddaef9a5cb31b8de7b22023f9b2d935317b7
alok9523 <97608103+alok9523@users.noreply.github.com>
2025-03-03 20:00:03 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+TOKEN = "7889602638:AAFtqbV5K_4uCEJcCL4DmdnkdJ0QCzVjrtg"  # Replace with your Telegram bot token
+
+# Database settings
+DB_PATH = "database/users.db"
+
+# File storage path
+FILE_STORAGE = "static/files/"
